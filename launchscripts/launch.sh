5224baac25d3e6ba903154fde84d1a008b0388b4
t0suj4 <valsiterb@gmail.com>
2021-03-06 11:36:59 +0100

    Updated launch scripts to print java version (#670)
    [skip ci]

--- launchscripts/launch.sh
+++ launchscripts/launch.sh
@@ -1,9 +1,12 @@
-#!/bin/sh -x
+#!/bin/sh
 
 # Server Launch Script
 #
 # Thrown together by Neeve in under five minutes, Public Domain
 # https://github.com/Neeve01 
+#
+# Fixed and added java version check by t0suj4, Public Domain
+# https://github.com/t0suj4
 
 # DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING
 FORGEJAR='{{forgeJar}}'
@@ -16,9 +19,20 @@ MAX_RAM='{{maxRAM}}'
 # DO NOT EDIT ANYTHING PAST THIS LINE
 LAUNCHPARAMS="-server -Xms$MIN_RAM -Xmx$MAX_RAM $JAVA_PARAMETERS -jar $FORGEJAR nogui"
 
-echo $'Launching the server...\n'
-echo '> java '$LAUNCHPARAMS$'\n'
+echo "Checking java version..."
+echo
+java -version
+echo
+echo "The expected java version is 1.8. Not higher, not lower."
+echo
+
+echo "Launching the server..."
+echo
+echo "> java $LAUNCHPARAMS"
 
 java $LAUNCHPARAMS
 
-read -p $'\n- The server has stopped. If it\'s a crash, please read the output above.\n\n- Press Return to exit...'
+echo
+echo "- The server has stopped. If it's a crash, please read the output above."
+echo
+read -p "- Press Return to exit..." _
