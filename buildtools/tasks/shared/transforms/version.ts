8f6242095b84abebe66fa25d92f0d41981724459
NotMyWing <winwyv@gmail.com>
2021-03-27 08:06:39 +1100

    Manage the modpack version field automatically

--- buildtools/tasks/shared/transforms/version.ts
+++ buildtools/tasks/shared/transforms/version.ts
@@ -12,18 +12,23 @@ export default async function transformManifestVersion(): Promise<void> {
 	let versionTitle;
 	if (process.env.GITHUB_TAG) {
 		const flavorTitle = process.env.BUILD_FLAVOR_TITLE;
+		const tag = process.env.GITHUB_TAG.replace(/^v/, "");
 
-		versionTitle = [modpackManifest.name, process.env.GITHUB_TAG.replace(/^v/, ""), flavorTitle]
-			.filter(Boolean)
-			.join(" - ");
+		versionTitle = [modpackManifest.name, tag, flavorTitle].filter(Boolean).join(" - ");
+
+		modpackManifest.version = tag;
 	}
 	// If SHA is provided and the build isn't tagged, append both the branch and short SHA.
 	else if (process.env.GITHUB_SHA && process.env.GITHUB_REF && process.env.GITHUB_REF.startsWith("refs/heads/")) {
 		const shortCommit = process.env.GITHUB_SHA.substr(0, 7);
 		const branch = /refs\/heads\/(.+)/.exec(process.env.GITHUB_REF);
 		versionTitle = `${modpackManifest.name} (${branch[1]} branch, ${shortCommit})`;
+
+		modpackManifest.version = `${branch}-${shortCommit}`;
 	} else {
 		versionTitle = `${modpackManifest.name} (manual build)`;
+
+		modpackManifest.version = "manual-build";
 	}
 
 	modpackManifest.name = versionTitle;
