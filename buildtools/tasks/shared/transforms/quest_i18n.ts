a7a81ce6b135a8606243e31ef0681ae1e29b52e7
NotMyWing <winwyv@gmail.com>
2021-12-30 00:58:46 +1100

    Minify the quest book file
    Makes no sense to have it elaborate
    Shrinks the file size by a factor of 2

--- buildtools/tasks/shared/transforms/quest_i18n.ts
+++ buildtools/tasks/shared/transforms/quest_i18n.ts
@@ -58,5 +58,5 @@ export default async function transfomLang(): Promise<void> {
 
 	await fs.promises.mkdir(questLangLocation, { recursive: true });
 	await fs.promises.writeFile(upath.join(questLangLocation, "en_us.lang"), lines.join("\n"));
-	return fs.promises.writeFile(questDatabasePath, JSON.stringify(questBook, null, 4));
+	return fs.promises.writeFile(questDatabasePath, JSON.stringify(questBook));
 }
