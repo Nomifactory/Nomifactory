9bed2da547383ba7ea54817c6e438dce7247f24d
NotMyWing <winwyv@gmail.com>
2022-11-15 03:49:38 +1100

    Triple-stache templates

--- launchscripts/launch.bat
+++ launchscripts/launch.bat
@@ -1,19 +1,19 @@
 :: Server Launch Script
 ::
 :: Thrown together by Neeve in under five minutes, Public Domain
-:: https://github.com/Neeve01 
+:: https://github.com/Neeve01
 ::
 :: Added java version check by t0suj4, Public Domain
 :: https://github.com/t0su4
 
 :: DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING
 @ECHO OFF
-SET FORGEJAR={{forgeJar}}
-SET JAVA_PARAMETERS={{jvmArgs}}
+SET FORGEJAR={{{forgeJar}}}
+SET JAVA_PARAMETERS={{{jvmArgs}}}
 
 :: these you can edit
-SET MIN_RAM={{minRAM}}
-SET MAX_RAM={{maxRAM}}
+SET MIN_RAM={{{minRAM}}}
+SET MAX_RAM={{{maxRAM}}}
 
 :: DO NOT EDIT ANYTHING PAST THIS LINE
 SET LAUNCHPARAMS=-server -Xms%MIN_RAM% -Xmx%MAX_RAM% %JAVA_PARAMETERS% -Dlog4j.configurationFile=log4j2_112-116.xml -jar %FORGEJAR% nogui
--- launchscripts/launch.sh
+++ launchscripts/launch.sh
@@ -3,18 +3,18 @@
 # Server Launch Script
 #
 # Thrown together by Neeve in under five minutes, Public Domain
-# https://github.com/Neeve01 
+# https://github.com/Neeve01
 #
 # Fixed and added java version check by t0suj4, Public Domain
 # https://github.com/t0suj4
 
 # DO NOT EDIT UNLESS YOU KNOW WHAT YOU'RE DOING
-FORGEJAR='{{forgeJar}}'
-JAVA_PARAMETERS='{{jvmArgs}}'
+FORGEJAR='{{{forgeJar}}}'
+JAVA_PARAMETERS='{{{jvmArgs}}}'
 
 # these you can edit
-MIN_RAM='{{minRAM}}'
-MAX_RAM='{{maxRAM}}'
+MIN_RAM='{{{minRAM}}}'
+MAX_RAM='{{{maxRAM}}}'
 
 # DO NOT EDIT ANYTHING PAST THIS LINE
 LAUNCHPARAMS="-server -Xms$MIN_RAM -Xmx$MAX_RAM $JAVA_PARAMETERS -Dlog4j.configurationFile=log4j2_112-116.xml -jar $FORGEJAR nogui"
